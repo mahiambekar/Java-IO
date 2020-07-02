c632d3c3491369260af886fc416aa7d0bde3b7e3
mahiambekar <66378229+mahiambekar@users.noreply.github.com>
2020-07-02 15:57:00 +0530

    Add files via upload

---
+++ ListOutNamesOfFilesNFolders.java
@@ -0,0 +1,18 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+import java.io.*;
+import java.util.Date;
+public class Main
+{
+	public static void main(String[] args) {
+		File f1 = new File("Text.txt");
+		String[] arr=f1.list();
+		for(String name: arr);
+		System.out.println(name);
+	}
+}
