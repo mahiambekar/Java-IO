21f04aae94a12738662ee0aceaaf9f0a3d43f127
mahiambekar <66378229+mahiambekar@users.noreply.github.com>
2020-07-02 15:56:19 +0530

    Add files via upload

---
+++ DeletingFile.java
@@ -0,0 +1,16 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+import java.io.*;
+import java.util.Date;
+public class Main
+{
+	public static void main(String[] args) {
+		File f1 = new File("Text.txt");
+		f1.delete();
+	}
+}
