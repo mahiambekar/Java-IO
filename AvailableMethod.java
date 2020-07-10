b658e87b0d931e5814cd4e713a575c92cf791969
mahiambekar <66378229+mahiambekar@users.noreply.github.com>
2020-07-10 20:01:36 +0530

    Update AvailableMethod.java

--- AvailableMethod.java
+++ AvailableMethod.java
@@ -4,7 +4,7 @@ import java.util.Date;
 public class Main
 {
 	public static void main(String[] args) {
-                  FileInputStream f1=new FileInputStream(text.txt)   
+                  FileInputStream f1=new FileInputStream(text.txt);  
                   int x;
                   System.out.println(f1.available());
                   f1.close();
