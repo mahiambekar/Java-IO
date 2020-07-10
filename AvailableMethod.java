c5fa731612330d4c04f3a9528cccaa4a5a21e0ac
mahiambekar <66378229+mahiambekar@users.noreply.github.com>
2020-07-10 19:51:17 +0530

    Create AvailableMethod.java

---
+++ AvailableMethod.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.Date;
+
+public class Main
+{
+	public static void main(String[] args) {
+                  FileInputStream f1=new FileInputStream(file name)   
+                  int x;
+                  System.out.println(f1.available());
+                  f1.close();
+        }
+}
