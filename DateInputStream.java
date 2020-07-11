ca6c9d5e692e8be460c65f983b9924c1db5072c3
mahiambekar <66378229+mahiambekar@users.noreply.github.com>
2020-07-11 18:33:57 +0530

    Add files via upload

---
+++ DateInputStream.java
@@ -0,0 +1,24 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+import java.io.*;
+import java.util.Date;
+public class Main
+{
+	public static void main(String[] args) {
+	    FileInputStream fin = new FileInputStream("test.txt");
+        DataInputStream din = new DataInputStream(fin);
+        int a = din.readInt();
+        double b = din.readDouble();
+        boolean c = din.readBoolean();
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        din.close();
+		
+	}
+}
