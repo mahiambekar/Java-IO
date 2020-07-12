8dedf1ee00b5e04f06279d0c32fd7eb0c78af964
mahiambekar <66378229+mahiambekar@users.noreply.github.com>
2020-07-12 10:01:25 +0530

    Add files via upload

---
+++ Console.java
@@ -0,0 +1,27 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+import java.io.*;
+public class Main
+{
+	public static void main(String[] args) throws Exception{
+	    String uname = "";
+        String pass = "";
+        Console con = System.console();
+        System.out.println("Enter Username");
+        uname = con.readLine();
+        System.out.println("Enter Password");
+        char[] arr = con.readPassword();
+        pass = new String(arr);
+        System.out.println("your username is" + uname);
+        System.out.println("your password is
+	   
+
+		
+
+	}
+}
